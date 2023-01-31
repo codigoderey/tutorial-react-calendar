58ebe7555c04ba63b4ce92e4cf6346d51cc9a3e2
Reynaldo Navedo <codigoderey@outlook.com>
2023-01-30 23:54:00 -0500

    recurrent dates added

---
+++ src/Recurrent.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+
+const Recurrent = () => {
+	const [value, onChange] = useState(new Date());
+
+	const onSaveDates = () => {
+		const totalWeeks = 12;
+
+		fetch("http://localhost:3000/days", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				date: new Date(value).toLocaleDateString()
+			})
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+
+		// loop to save the dates in the database
+		for (let i = 0; i < totalWeeks; i++) {
+			value.setDate(value.getDate() + 7);
+			fetch("http://localhost:3000/days", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					date: value.toLocaleDateString()
+				})
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+		}
+	};
+
+	return (
+		<div className="full-screen flex">
+			<Calendar calendarType="US" value={value} onChange={onChange} />
+			<button onClick={onSaveDates}>Save</button>
+		</div>
+	);
+};
+
+export default Recurrent;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'react-calendar/dist/Calendar.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Recurrent from "./Recurrent";
+import "./index.css";
+import "react-calendar/dist/Calendar.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />
+	},
+	{
+		path: "/recurrent",
+		element: <Recurrent />
+	}
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
